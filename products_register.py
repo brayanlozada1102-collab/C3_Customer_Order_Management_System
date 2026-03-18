269b699b8f9ee5d82202e8dbf5efe1e0676ebedb
dilandcv <josemiguelvargas_@hotmail.com>
2026-03-18 10:49:29 -0500

    sss

--- products_register.py
+++ products_register.py
@@ -6,10 +6,11 @@ products={
 
 def register_product(products, name, price):
     product_id = len(products) + 1
-    product = (product_id, name, price)
+    product = (name, price)
     products[product_id] = product
     return products
 
+ 
 name = input("enter the product name: ")
 price = float(input("enter the product price: "))
 products = register_product(products,name, price )
