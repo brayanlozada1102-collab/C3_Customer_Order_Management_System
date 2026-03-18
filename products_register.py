390b32c9f2445539a61e2749ef8c521cc2692d28
dilandcv <josemiguelvargas_@hotmail.com>
2026-03-18 10:35:36 -0500

    adelanto del codigo

---
+++ products_register.py
@@ -0,0 +1,18 @@
+products={
+        1:('coffe',1500),
+        2:('azucar',2000)
+    }
+
+
+def register_product(products, name, price):
+    product_id = len(products) + 1
+    product = (product_id, name, price)
+    products[product_id] = product
+    return products
+
+name = input("enter the product name: ")
+price = float(input("enter the product price: "))
+products = register_product(products,name, price )
+print(products)
+
+
